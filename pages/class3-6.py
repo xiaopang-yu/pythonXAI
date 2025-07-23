728b63fc8adee65bdf0502f5430d5dcf870f025f
xiaopang-yu <claire20031102@gmail.com>
2025-07-23 15:47:00 +0800

    重構一番賞練習程式，修正邏輯以使用隨機抽取並記錄已抽過的格子，提升遊戲體驗

--- pages/class3-6.py
+++ pages/class3-6.py
@@ -1,13 +1,24 @@
+# 一番賞練習
 import random
 
-prizeA = int(input("請輸入Ａ賞編號"))
+table = []
+for i in range(100):
+    table.append(0)
 
+target = random.randint(1, 100)
+table[target - 1] = 1  # 要 -1 是因為 table 是 0 到 99 ，target 是 1 到 100
+
+count = 0
 while True:
-    guess = int(random.randint(1, 100))
-    count = 0
-    if prizeA != guess:
-        count += 1
-    else:
-        money = 200 * count
-        print(f"你在第{count}抽抽到Ａ賞，共花費{money}元")
+    pick = random.randint(1, 100)
+    count += 1
+    if table[pick - 1] == 2:
+        print("這格已經抽過了")
+        count -= 1
+    elif table[pick - 1] == 1:
+        print("恭喜中獎")
         break
+    else:
+        print("沒有中獎，請繼續抽")
+    table[pick - 1] = 2
+print(f"你在第{count}抽抽到Ａ賞，共花費{count*200}元")
