d75f467eb1365d32fce76e0c68a8b2e7da3e2f1a
xiaopang-yu <claire20031102@gmail.com>
2025-07-24 11:58:59 +0800

    Add shopping interface with product display and purchase functionality

---
+++ pages/class4-3.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import os
+import time
+
+st.title("SWEETY・SHOOPING")
+
+if "products" not in st.session_state:
+    st.session_state.products = {}
+
+col_num = st.number_input("請輸入欄位數", min_value=1, max_value=5, value=3, step=1)
+
+file_path = "image"
+files = os.listdir(file_path)
+
+for img in files:
+    if img[:-4] not in st.session_state.products:
+        st.session_state.products[img[:-4]] = {
+            "image": f"{file_path}/{img}",
+            "price": 10,
+            "stock": 10,
+        }
+
+cols = st.columns(col_num)
+
+index = 0
+for name, detail in st.session_state.products.items():
+    with cols[index % col_num]:
+        st.image(
+            st.session_state.products[name]["image"], use_container_width=True
+        )  # use_container_width = True 表示欄位大小＝圖片大小
+        st.write(" #### " + name)
+        st.write("price : $ " + str(st.session_state.products[name]["price"]))
+        st.write("stock :  " + str(st.session_state.products[name]["stock"]))
+        if st.button("buy " + name):
+            st.session_state.products[name]["stock"] -= 1
+            st.success(f"已購買 {name}")
+            time.sleep(1)
+            st.rerun()
+    index += 1
